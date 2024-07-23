f9f4cfc19bbb37ce9cc4756e0c881cbab8923322
dipishBisht <dipish1718@gmail.com>
2024-07-23 15:16:29 +0530

    headers added

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import HomeHeader from "./components/Other Components/Header";
 import Footer from "./components/Other Components/Footer";
 import Navbar from "./components/Other Components/Navbar";
 import LocomotiveScroll from "locomotive-scroll";
-// import { FloatingNavDemo } from "./components/Other Components/FloatingNavDemo";
+import { AboutHeader } from "./components/Other Components/AboutHeader";
 import { NavbarDemo } from "./components/Other Components/NavbarDemo";
+import { WebHeader } from "./components/Other Components/WebHeader";
+import { AppHeader } from "./components/Other Components/AppHeader";
+import { GameHeader } from "./components/Other Components/GameHeader";
+import { DigitalHeader } from "./components/Other Components/DigitalHeader";
+import { GraphicHeader } from "./components/Other Components/GraphicHeader";
 
 const App: React.FC = () => {
   const locomotiveScroll = new LocomotiveScroll();
@@ -14,10 +19,23 @@ const App: React.FC = () => {
   return (
     <div className="overflow-x-hidden font-[Adamina] bg-[#000]">
       <Navbar />
-      {/* <FloatingNavDemo /> */}
       <NavbarDemo />
-      {curPage === "/" ? <HomeHeader /> : <>Other Page Headers</>}
-      <main className="w-full mx-auto flex flex-col gap-[100px] py-12 my-[50px] bg-black z-30 relative">
+      {curPage === "/" ? (
+        <HomeHeader />
+      ) : curPage === "/about" ? (
+        <AboutHeader />
+      ) : curPage === "/services/web" ? (
+        <WebHeader />
+      ) : curPage === "/services/app" ? (
+        <AppHeader />
+      ) : curPage === "/services/game" ? (
+        <GameHeader />
+      ) : curPage === "/services/digital" ? (
+        <DigitalHeader />
+      ) : (
+        <GraphicHeader />
+      )}
+      <main className="w-full mx-auto flex flex-col py-12 my-[50px] bg-black z-30 relative text-white">
         <Outlet />
       </main>
       <Footer />
